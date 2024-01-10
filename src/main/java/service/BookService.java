e2a60e309224101e5dfcd81aa871c6cddc0ef5a6
hossein javadi <hosseinj13@yahoo.com>
2024-01-11 02:40:07 +0330

    update BookService.java

--- src/main/java/service/BookService.java
+++ src/main/java/service/BookService.java
@@ -4,7 +4,9 @@ import model.Author;
 import model.Book;
 import repository.BookRepository;
 import repository.AuthorRepository;
+
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.List;
 
 public class BookService {
@@ -19,28 +21,32 @@ public class BookService {
 
 
     public void addBook(String title, int publicationYear, int authorId) throws SQLException {
-
-        Book book = new Book(title, publicationYear, authorId);
-        bookRepository.save(book);
+        Book bookToAdd = new Book(title, publicationYear, authorId);
+        bookRepository.save(bookToAdd);
         /*
         // گرفتن نویسنده با استفاده از شناسه
         Author author = authorRepository.load(authorId);
-        // بررسی اینکه نویسنده واقعی وجود دارد یا خیر
         if (author != null) {
-            Book book = new Book(title, publicationYear, authorId);
-            bookRepository.save(book);
-            // افزودن کتاب به لیست کتب نویسنده
-            List<Book> authorBooks = author.getBooks();
-            authorBooks.add(book);
+            Book bookToAdd = new Book(title, publicationYear, authorId);
+            bookRepository.save(bookToAdd);
+            // افزودن کتاب به آرایه کتب نویسنده
+            Book[] authorBooks = author.getBooks();
+            int newSize = (authorBooks != null) ? authorBooks.length + 1 : 1;
+            assert authorBooks != null;
+            Book[] updatedBooks = Arrays.copyOf(authorBooks, newSize);
+            updatedBooks[newSize - 1] = bookToAdd;
+            author.setBooks(updatedBooks);
+
             // چاپ اطلاعات نویسنده و کتب مرتبط با او
             System.out.println("Author: " + author);
             System.out.println("Books:");
-            for (Book authorBook : authorBooks) {
-                System.out.println(authorBook);
+            for (Book book : updatedBooks) {
+                System.out.println(book);
             }
         } else {
             System.out.println("Author not found with ID: " + authorId);
         }*/
+
     }
 
 

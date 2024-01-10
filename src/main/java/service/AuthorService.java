6ebf9172fa059262a89a0b0ad8457ea21c98b85c
hossein javadi <hosseinj13@yahoo.com>
2024-01-10 19:50:38 +0330

    update register method AuthorService.java  class

--- src/main/java/service/AuthorService.java
+++ src/main/java/service/AuthorService.java
@@ -1,10 +1,22 @@
 package service;
 
+import model.Author;
+import repository.AuthorRepository;
+
+import java.sql.SQLException;
 import java.util.Date;
 
 public class AuthorService {
 
-    public void register(String firstname, String lastname, Date age){
+    private final AuthorRepository authorRepository = new AuthorRepository();
+
+    public AuthorService() throws SQLException {
+    }
+
+
+    public void register(String firstname, String lastname, Date age) throws SQLException {
+        Author author = new Author(firstname, lastname, age);
+        authorRepository.save(author);
 
     }
 }

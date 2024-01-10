e34dab90752ac2f541f9642cf94ccf2108f1f5b6
hossein javadi <hosseinj13@yahoo.com>
2024-01-10 17:55:49 +0330

    create jdbcConnection.java  class

---
+++ src/main/java/jdbcConnection.java
@@ -0,0 +1,15 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class jdbcConnection {
+
+    private final Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/hw7", "postgres","h9311240246N");
+
+    public jdbcConnection() throws SQLException {
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+}

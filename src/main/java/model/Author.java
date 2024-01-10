eabf644bc957b8ac052888c55dffa791bfa33bc5
hossein javadi <hosseinj13@yahoo.com>
2024-01-10 17:04:23 +0330

    create  and add fields in Author.java

--- src/main/java/model/Author.java
+++ src/main/java/model/Author.java
@@ -1,4 +1,12 @@
 package model;
 
+import java.util.Date;
+import java.util.List;
+
 public class Author {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private Date age;
+    private List<Book> books;
 }

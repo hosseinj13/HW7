0a154ad8443c8c29b3c310d6f30aa105e823b4f5
hossein javadi <hosseinj13@yahoo.com>
2024-01-10 22:42:30 +0330

    update and use the AuthorService.java and BookRepository.java in the Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,29 @@
 package org.example;
 
+import repository.BookRepository;
+import service.AuthorService;
+import service.BookService;
+import model.*;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.time.LocalDate;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws SQLException, ParseException {
+
+        AuthorService authorService = new AuthorService();
+        authorService.register("hasan", "javadi", Date.valueOf(LocalDate.of(1990, 1, 24)));
+        //authorService.findAuthor(19);
+
+
+        BookService bookService = new BookService();
+        bookService.addBook("ketab", 1300, 1);
+        //bookService.findBook(6);
+        //bookService.deleteBook(6);
+
 
     }
 }

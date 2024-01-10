1469bccb62e514f0bb210c21e648bbc94b292008
hossein javadi <hosseinj13@yahoo.com>
2024-01-11 02:40:50 +0330

    update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,6 @@ import repository.BookRepository;
 import service.AuthorService;
 import service.BookService;
 import model.*;
-
 import java.sql.Date;
 import java.sql.SQLException;
 import java.text.ParseException;
@@ -16,14 +15,13 @@ public class Main {
     public static void main(String[] args) throws SQLException, ParseException {
 
         AuthorService authorService = new AuthorService();
-        authorService.register("javid", "javadi", Date.valueOf(LocalDate.of(1850, 2, 14)));
-        authorService.findAuthor(19);
-
+       // authorService.register("javid", "javadi", Date.valueOf(LocalDate.of(1850, 2, 14)));
+        //authorService.findAuthor(19);
 
         BookService bookService = new BookService();
-        bookService.addBook("ketab", 1300, 1);
-        bookService.findBook(6);
-        bookService.deleteBook(6);
+        bookService.addBook("The Great Gatsby", 1925, 7);
+       // bookService.findBook(6);
+       // bookService.deleteBook(6);
 
 //        Author author = new Author("Ali", "Shariati", Date.valueOf(LocalDate.of(1850, 2, 14)));
 //        authorService.register(author.getFirstName(), author.getLastName(), author.getAge());

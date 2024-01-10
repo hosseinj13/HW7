ef37ea60514632afaf370ec60428b205eda1aeb9
hossein javadi <hosseinj13@yahoo.com>
2024-01-10 19:49:43 +0330

    update save method AuthorRepository.java.java  class

--- src/main/java/repository/AuthorRepository.java
+++ src/main/java/repository/AuthorRepository.java
@@ -2,9 +2,34 @@ package repository;
 
 import model.Author;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import org.example.jdbcConnection;
+
 public class AuthorRepository {
 
-    public void save(Author author){
+    private final jdbcConnection jdbcConnection = new jdbcConnection();
+
+    public AuthorRepository() throws SQLException {
+    }
+
+
+    public void save(Author author) throws SQLException {
+        Connection connection = jdbcConnection.getConnection();
+
+        String addAuthor = "INSERT INTO  authors(first_name, last_name, age) VALUES (?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(addAuthor);
+
+        preparedStatement.setString(1, author.getFirstName());
+        preparedStatement.setString(2, author.getLastName());
+        preparedStatement.setDate(3, (Date) author.getAge());
+        int result = preparedStatement.executeUpdate();
+        if (result == 1)
+            System.out.println(author.getFirstName() + " " + author.getLastName() + " successfully register :)");
+        else
+            System.out.println("somethings is wrong :(");
 
     }
 
